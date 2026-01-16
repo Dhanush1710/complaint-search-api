6b14febe9c3527922515d412c369d8e106291719
Dhanush <dhanushnagarajan17@gmail.com>
2026-01-15 20:55:26 -0500

    Bind Flask to Render port

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -34,6 +35,6 @@ def search_complaints():
         </body>
     </html>
     """
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
